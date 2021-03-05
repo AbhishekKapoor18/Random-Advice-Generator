8a0656ddec73d1609de236b10b5bc9ec06bdb68b
Abhishek Kapoor <AbhishekKapoor1006@gmail.com>
2021-03-05 03:39:46 -0500

    Fetch API small project

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+
+const button = document.querySelector('button');
+const header = document.querySelector('h1');
+
+button.addEventListener('click', () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then(result => result.json())
+    .then(data => {
+        console.log(header.innerText = data.slip.advice);
+    });
+});
+
